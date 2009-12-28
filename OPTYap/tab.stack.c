2e79f8d02033aabdbb00530a95dfed311928cb03
Flávio Cruz <flaviocruz@gmail.com>
2009-12-28 21:08:19 +0000

    Add tab.stack files

---
+++ OPTYap/tab.stack.c
@@ -0,0 +1,10 @@
+/**********************************************************************
+                                                               
+                       The OPTYap Prolog system                
+  OPTYap extends the Yap Prolog system to support or-parallel tabling
+                                                               
+  Copyright:   R. Rocha and NCC - University of Porto, Portugal
+  File:        opt.proto.h
+                                                                     
+**********************************************************************/
+
