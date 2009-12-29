f7a24cc7d15f87adee9937abbf8817f1d1497878
Flávio Cruz <flaviocruz@gmail.com>
2009-12-29 21:58:56 +0000

    use tstTermStack

--- OPTYap/tab.subsumption.c
+++ OPTYap/tab.subsumption.c
@@ -13,7 +13,7 @@
 ** ------------------ */
 
 #include "Yap.h"
-#if defined(TABLING) && defined(TABLING_CALL_SUBSUMPTION)
+#ifdef TABLING_CALL_SUBSUMPTION
  
 #include <stdio.h>
 #ifdef HAVE_STRING_H
@@ -24,32 +24,8 @@
 #include "yapio.h"
 #include "tab.macros.h"
 #include "tab.stack.h"
-
-#if 0
-#define SUBSUMPTION_CPSTACK_SIZE 1024
-
-typedef struct {
-  int index;             /* location within the tstTermStack... */
-  CELL* value;            /* where this value appeared. */
-} stack_term_log_frame;
-
-typedef struct {
-  // trilha de variaveis aqui!
-  int term_log_index;
-  CELL* stack_term_top; /* stack term top in the moment of frame creation */
-  sg_node_ptr alt_node;
-} subsumption_search_frame;
-
-struct subsumption_search_stack {
-  subsumption_search_frame *top; /* current frame */
-  subsumption_search_frame *ceiling; /* frame max */
-  subsumption_search_frame base[SUBSUMPTION_CPSTACK_SIZE];
-};
-
-static stack_term_log_frame stack_term_log[SUBSUMPTION_CPSTACK_SIZE];
-static CELL* subsumption_vars[MAX_TABLE_VARS];
-static struct subsumption_search_stack subsumption_stack;
-#endif
+#include "tab.xsb.h"
+#include "tab.utils.h"
 
 typedef enum {
   TAG_ATOM,
@@ -100,22 +76,14 @@ CellTag cell_tag(Term t)
   return TAG_UNKNOWN;
 }
 
-static int stack_terms_init = FALSE;
-static DynamicStack stack_terms;
-
 void subsumptive_search(yamop *preg, CELL **Yaddr)
 {
-  int i, j, count_vars, arity;
+  int i, count_vars, arity;
   CELL *stack_vars;
   tab_ent_ptr tab_ent;
   sg_node_ptr current_sg_node;
   
-  if(!stack_terms_init) {
-    DynStk_Init(&stack_terms, 2048, Term, "stack_terms");
-    stack_terms_init = TRUE;
-  }
-  
-  DynStk_ResetTOS(stack_terms);
+  TermStack_ResetTOS;
   
   printf("subsumptive_search(preg, Yaddr)\n");
   
@@ -125,18 +93,15 @@ void subsumptive_search(yamop *preg, CELL **Yaddr)
   stack_vars = *Yaddr;
   current_sg_node = TabEnt_subgoal_trie(tab_ent);
   
-  Term* frame;
   for (i = arity; i > 0; --i) {
-    DynStk_Push(stack_terms, frame);
-    *(Term*)frame = Deref(XREGS[i]);
+    TermStack_Push(Deref(XREGS[i]));
   } 
   
   Term t;
   
   do {
-    DynStk_Pop(stack_terms, frame);
-    
-    t = *frame;
+    TermStack_Pop(t);
+  
     switch(cell_tag(t)) {
       case TAG_REF:
         printf("VAR ");
@@ -150,8 +115,10 @@ void subsumptive_search(yamop *preg, CELL **Yaddr)
       case TAG_STRUCT:
         printf("Appl ");
         break;
+      default:
+        break;
     }
-  } while (!DynStk_IsEmpty(stack_terms));
+  } while (!TermStack_IsEmpty);
 
   printf("\n");
 }
