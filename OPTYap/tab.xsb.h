f3a6e182ed6ec0173da992b285bfbbd984a2fc96
Flávio Cruz <flaviocruz@gmail.com>
2009-12-29 21:49:34 +0000

    add tab.xsb.h. file with xsb macros

---
+++ OPTYap/tab.xsb.h
@@ -0,0 +1,22 @@
+/**********************************************************************
+                                                               
+                       The OPTYap Prolog system                
+  OPTYap extends the Yap Prolog system to support or-parallel tabling
+                                                               
+  Copyright:   R. Rocha and NCC - University of Porto, Portugal
+  File:        tab.xsb.h
+                                                                     
+**********************************************************************/
+
+#ifndef TAB_XSB_H
+#define TAB_XSB_H
+
+#include "opt.config.h"
+
+#ifdef TABLING_CALL_SUBSUMPTION
+
+typedef Term *CPtr;
+
+#endif /* TABLING_CALL_SUBSUMPTION */
+
+#endif
