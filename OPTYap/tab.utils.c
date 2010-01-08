373d1d5ac52dfdeaa5ddee2622a56afaf8a2a201
Flávio Cruz <flaviocruz@gmail.com>
2010-01-08 01:08:54 +0000

    preliminary version of printTriePath.

--- OPTYap/tab.utils.c
+++ OPTYap/tab.utils.c
@@ -11,6 +11,7 @@
 #include "Yap.h"
 #include "tab.utils.h"
 #include "Yatom.h"
+#include "tab.macros.h"
 
 #ifdef TABLING_CALL_SUBSUMPTION
 
@@ -47,6 +48,33 @@ CellTag cell_tag(Term t)
   }
   
   return TAG_UNKNOWN;
+}
+
+static inline
+CellTag TrieSymbolType(Term t)
+{
+  if(IsVarTerm(t))
+    return XSB_TrieVar;
+    
+  if(IsIntTerm(t))
+    return TAG_INT;
+  
+  if(IsAtomTerm(t))
+    return TAG_ATOM;
+  
+  if(IsPairTerm(t))
+    return TAG_LIST;
+  
+  if(IsApplTerm(t)) {
+    Functor f = (Functor) RepAppl(t);
+    if (f == FunctorDouble)
+      return TAG_FLOAT;
+    
+    if(f == FunctorLongInt)
+      return TAG_LONG_INT;
+    
+    return TAG_STRUCT;
+  }
 }
 
 xsbBool are_identical_terms(Cell term1, Cell term2)
@@ -90,6 +118,167 @@ begin_are_identical_terms:
   }
   else return FALSE;
   // TODO: long int e double
+}
+
+static
+void printTrieNode(FILE *fp, BTNptr pTN) {
+  fprintf(fp, "Trie Node: Addr(%p)", pTN);
+  // XXX
+}
+
+void printTrieSymbol(FILE* fp, Cell symbol) {
+  if(symbol == ESCAPE_NODE_SYMBOL)
+    fprintf(fp, "%lu [ESCAPE_NODE_SYMBOL]", ESCAPE_NODE_SYMBOL);
+  else {
+    switch(TrieSymbolType(symbol)) {
+      case XSB_INT:
+        fprintf(fp, IntegerFormatString, int_val(symbol));
+        break;
+      case XSB_FLOAT:
+        /// XXXX
+        break;
+      case XSB_STRING:
+        fprintf(fp, "%s", string_val(symbol));
+        break;
+      case XSB_TrieVar:
+        fprintf(fp, "V" IntegerFormatString, DecodeTrieVar(symbol));
+        break;
+      case XSB_STRUCT:
+        {
+          Psc psc;
+          /// XXX Float??
+          psc = DecodeTrieFunctor(symbol);
+          fprintf(fp, "%s/%d", get_name(psc), get_arity(psc));
+          break;
+        }
+      case XSB_LIST:
+        // TODO
+        break;
+      default:
+        fprintf(fp, "Unknown symbol (tag = %ld)", cell_tag(symbol));
+        break;
+    }
+  }
+}
+
+static
+void symstkPrintNextTerm(CTXTdeclc FILE *fp, xsbBool list_recursion) {
+  Cell symbol;
+  
+  if(SymbolStack_IsEmpty) {
+    fprintf(fp, "<no subterm>");
+    return;
+  }
+  
+  SymbolStack_Pop(symbol);
+  
+  switch(TrieSymbolType(symbol)) {
+    case XSB_INT:
+      if(list_recursion)
+        fprintf(fp, "|" IntegerFormatString "]", int_val(symbol));
+      else
+        fprintf(fp, IntegerFormatString, int_val(symbol));
+      break;
+    case XSB_FLOAT:
+      // XXX
+      break;
+    case XSB_STRING:
+      {
+        char *string = string_val(symbol);
+        
+        if(list_recursion) {
+          if(symbol == TermNil) /// XXX Diferente!
+            fprintf(fp, "]");
+          else
+            fprintf(fp, "|%s]", string);
+        }
+        else
+          fprintf(fp, "%s", string);
+      }
+      break;
+    case XSB_TrieVar:
+      if(list_recursion)
+        fprintf(fp, "|V" IntegerFormatString "]", DecodeTrieVar(symbol));
+      else
+        fprintf(fp, "V" IntegerFormatString, DecodeTrieVar(symbol));
+      break;
+    case XSB_STRUCT:
+      {
+        Psc psc;
+        
+        int i;
+        
+        /// Boxed float XXX
+        if(list_recursion)
+          fprintf(fp, "|");
+        psc = DecodeTrieFunctor(symbol);
+        
+        fprintf(fp, "%s(", get_name(psc));
+        for(i = 1; i < (int)get_arity(psc); i++) {
+          symstkPrintNextTerm(CTXTc fp, FALSE);
+          fprintf(fp, ",");
+        }
+        symstkPrintNextTerm(CTXTc fp, FALSE);
+        fprintf(fp, ")");
+        if(list_recursion)
+          fprintf(fp, "]");
+      }
+      break;
+    case XSB_LIST:
+    ///XXX
+    break;
+    default:
+      fprintf(fp, "<unknown symbol>");
+      break;
+  }
+}
+
+void printTriePath(CTXTdeclc FILE *fp, BTNptr pLeaf, xsbBool printLeafAddr)
+{
+  BTNptr pRoot;
+  
+  if(IsNULL(pLeaf)) {
+    fprintf(fp, "NULL");
+    return;
+  }
+  
+  /*if(!IsLeafNode(pLeaf)) { // XXX
+    fprintf(fp, "printTriePath() called with non-Leaf node!\n");
+    printTrieNode(fp, pLeaf);
+    return;
+  }*/
+  
+  if(printLeafAddr)
+    fprintf(fp, "Leaf %p: ", pLeaf);
+  
+  if(IsEscapeNode(pLeaf)) {
+    pRoot = BTN_Parent(pLeaf);
+    if(IsNonNULL(pRoot))
+      printTrieSymbol(fp, BTN_Symbol(pRoot));
+    else
+      fprintf(fp, "ESCAPE node");
+    return;
+  }
+  
+  SymbolStack_ResetTOS;
+  SymbolStack_PushPathRoot(pLeaf, pRoot);
+  //printf("Num symbols: %d", SymbolStack_NumSymbols);
+  
+  if(IsTrieFunctor(BTN_Symbol(pRoot))) {
+    //printf("Is trieFunctor!!\n");
+    SymbolStack_Push(BTN_Symbol(pRoot));
+    symstkPrintNextTerm(CTXTc fp, FALSE);
+  }
+  else {
+    //printTrieSymbol(fp, BTN_Symbol(pRoot));
+    fprintf(fp, "(");
+    symstkPrintNextTerm(CTXTc fp, FALSE);
+    while(!SymbolStack_IsEmpty) {
+      fprintf(fp, ",");
+      symstkPrintNextTerm(CTXTc fp, FALSE);
+    }
+    fprintf(fp, ")");
+  }
 }
 
 #endif /* TABLING_CALL_SUBSUMPTION */
