aaffb8457808a7f466960fafcbe99525d1c0aef5
Flávio Cruz <flaviocruz@gmail.com>
2010-02-16 14:58:36 +0000

    add tab.unify.c and tab.unify.h

---
+++ OPTYap/tab.unify.c
@@ -0,0 +1,11 @@
+/**********************************************************************
+                                                               
+                       The OPTYap Prolog system                
+  OPTYap extends the Yap Prolog system to support or-parallel tabling
+                                                               
+  Copyright:   Flávio Cruz
+  File:        tab.unify.c
+  version:        
+  comment:     from XSB's emu/tst_unify.c                                
+**********************************************************************/
+
---
+++ OPTYap/tab.unify.h
@@ -0,0 +1,10 @@
+/**********************************************************************
+                                                               
+                       The OPTYap Prolog system                
+  OPTYap extends the Yap Prolog system to support or-parallel tabling
+                                                               
+  Copyright:   Flávio Cruz
+  File:        tab.unify.h
+  version:        
+  comment:                                                
+**********************************************************************/
