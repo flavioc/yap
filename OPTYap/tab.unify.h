31a7d45546931699cf79afa2bd6b20bc8101d8bc
Flávio Cruz <flaviocruz@gmail.com>
2010-02-16 23:21:00 +0000

    import code tst_unify.c from XSB, with modifications to enable Yap compilation

--- OPTYap/tab.unify.h
+++ OPTYap/tab.unify.h
@@ -8,3 +8,23 @@
   version:        
   comment:                                                
 **********************************************************************/
+
+#ifndef TAB_UNIFY_H
+#define TAB_UNIFY_H
+
+/* ------------------ **
+**      Includes      **
+** ------------------ */
+#include "opt.config.h"
+
+#ifdef TABLING
+
+#include "tab.xsb.h"
+#include "tab.utils.h"
+#include "tab.tst.h"
+
+void consume_subsumptive_answer(CTXTdeclc BTNptr pAnsLeaf, int sizeTmplt, CPtr pAnsTmplt);
+
+#endif /* TABLING */
+
+#endif
